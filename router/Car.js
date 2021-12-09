f65a367419d1af5fe7ed966f3ffd7ead6ddc7b44
leith <leith.mhf@gmail.com>
2021-12-09 02:55:04 +0100

    leith-commit-car-router-ffix

--- router/Car.js
+++ router/Car.js
@@ -4,7 +4,7 @@ const carController = require("../controllers/Car");
 const isAuth = require("../middleware/isAuth");
 
 route.post("/addCar", isAuth, carController.addCar);
-route.get("/car/:page", carController.getCars);
+route.get("/cars/:page", carController.getCars);
 route.delete("/delete/:carId", isAuth, carController.deleteCar);
 
 module.exports = route;
